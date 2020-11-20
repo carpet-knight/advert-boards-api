4e1992fe19c15034f67246f87d2300c41a50d8d6
Ivan Vasilev <vanokvas@gmail.com>
2020-11-20 23:02:06 +0300

    Added common utils for fetching and parsing data & updated config with parser settings

--- app/config/default.js
+++ app/config/default.js
@@ -2,5 +2,15 @@ module.exports = {
     server: {
         host: '127.0.0.1',
         port: 9000
+    },
+    app: {
+        parserSettings: {
+            locator: {},
+            errorHandler: {
+                warning: (w) => { }, // suppress warnings
+                error: (e) => { },  // suppress minor errors
+                fatalError: (e) => console.error(e)
+            }
+        }
     }
 }
---
+++ app/utils/common.js
@@ -0,0 +1,26 @@
+const fetch = require('node-fetch');
+const { DOMParser } = require('xmldom');
+const { app } = require('../config/default');
+
+const parser = new DOMParser(app.parserSettings);
+
+const DEFAULT_HEADERS = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36'
+}
+
+async function getContent(url) {
+    const res = await fetch(url, {
+        headers: DEFAULT_HEADERS
+    });
+
+    return await res.text();
+}
+
+function formDocument(html) {
+    return parser.parseFromString(html);
+}
+
+module.exports = {
+    getContent,
+    formDocument
+}
